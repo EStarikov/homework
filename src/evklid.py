a7af401583227a456a95ce27f9551cd9a1ecc6d3
Egor Starikov <egor.starikov2007@gmail.com>
2025-09-18 23:33:37 +0300

    Рабочий файл Расширенного алгоритма Евклида

--- src/evklid.py
+++ src/evklid.py
@@ -1,2 +1,11 @@
-for i in range(5):
-    print("Hello world")
+def gcd(a, b):
+    if a == 0: return(b, 0, 1)
+
+    else: div, x, y = gcd(b%a, a)
+
+    return(div, y - (b//a)*x, x) 
+
+
+num1 = int(input())
+num2 = int(input())
+print(gcd(num1, num2))
