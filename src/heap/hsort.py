79b1158b6148399197209a5b66e6424c9761e891
Egor Starikov <egor.starikov2007@gmail.com>
2025-12-04 00:47:36 +0300

    Исправлены замечания

--- src/heap/hsort.py
+++ src/heap/hsort.py
@@ -13,14 +13,10 @@ def tree(arr, n, i):
 
 def h_sort(arr):
     l = len(arr)
+    for i in range(l//2, -1, -1):
+        tree(arr, l, i)
     while l - 1 > 0:
-        for i in range(l//2, -1, -1):
-            tree(arr, l, i)
         arr[0], arr[l-1] = arr[l-1], arr[0]
         l -= 1
+        tree(arr, l, 0) 
     return arr
-
-
-a = []
-
-print(h_sort(a))
