54c21efbb232fbadd8d1a979ba7c957136acce62
Egor Starikov <egor.starikov2007@gmail.com>
2025-12-11 23:26:53 +0300

    добавлены тесты

--- src/double.c
+++ src/double.c
@@ -1,10 +1,4 @@
-#include <math.h>
-#include <stdio.h>
-
-typedef union Number {
-    double value;
-    unsigned char binaryForm[8];
-} Number;
+#include "double.h"
 
 void From10To2(int* arr, int d)
 {
@@ -25,32 +19,31 @@ __uint64_t From2To10(int* arr, int l)
     return n;
 }
 
-int main()
+void bin(int* array, Number* a)
 {
-    int s;
-    double m;
-    int p;
-    Number n;
-    scanf("%lf", &n.value);
-    int binary[64];
-    int bip[11];
-    int bim[52];
     for (int i = 0; i <= 7; i++) {
         int arr[8];
-        From10To2(arr, n.binaryForm[i]);
+        From10To2(arr, a->binaryForm[i]);
         for (int j = 0; j <= 7; j++) {
-            binary[56 - i * 8 + j] = arr[j];
+            array[56 - i * 8 + j] = arr[j];
         }
     }
-    s = binary[0];
+}
+
+int order(int* arr)
+{
+    int bip[11];
     for (int i = 1; i < 12; i++) {
-        bip[i - 1] = binary[i];
+        bip[i - 1] = arr[i];
     }
-    p = From2To10(bip, 11) - 1023;
+    return From2To10(bip, 11) - 1023;
+}
+
+double mantissa(int* arr)
+{
+    int bim[52];
     for (int i = 0; i < 52; i++) {
-        bim[i] = binary[i + 12];
+        bim[i] = arr[i + 12];
     }
-    m = (double)From2To10(bim, 52) / (1ULL << 52) + 1.0;
-    printf("(-1)^%d * %.10f * 2^%d", s, m, p);
-    return 0;
+    return (double)From2To10(bim, 52) / (1ULL << 52) + 1.0;
 }
---
+++ src/double.h
@@ -0,0 +1,12 @@
+#include <stdint.h>
+
+typedef union Number {
+    double value;
+    unsigned char binaryForm[8];
+} Number;
+
+void From10To2(int* arr, int d);
+__uint64_t From2To10(int* arr, int l);
+void bin(int* array, Number* a);
+int order(int* arr);
+double mantissa(int* arr);
