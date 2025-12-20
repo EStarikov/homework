f23c71f191780259da78939384985eb3299334ad
Egor Starikov <egor.starikov2007@gmail.com>
2025-12-20 11:56:18 +0300

    Замечания исправлены, кроме сравнения n с argcount, n это арность, ее нужно передавать правильно, частичное каррирование реализовано с помощью args1, который фиксирует параметры заранее

--- src/curry.py
+++ src/curry.py
@@ -2,14 +2,15 @@ def sum3(a, b, c):
     return a + b + c
 
 
-def curry(func, n):
+def curry(func, n, *args1):
     if n != func.__code__.co_argcount:
-        raise Exception("неправильная арность")
-        return
+        raise ValueError
 
     def curried(*args):
-        if len(args) >= n:
-            return func(*args)
+        if len(args + args1) > n:
+            raise ValueError
+        elif len(args + args1) == n:
+            return func(*(args1 + args))
         else:
             return lambda *args2: curried(*(args + args2))
     return curried
@@ -17,6 +18,8 @@ def curry(func, n):
 
 def uncurry(func, n):
     def uncurried(*args):
+        if len(args) != n:
+            raise ValueError
         result = func
         for arg in args:
             result = result(arg)
@@ -24,7 +27,7 @@ def uncurry(func, n):
     return uncurried
 
 
-sum3_curry = curry(sum3, 3)
-sum3_uncurry = uncurry(sum3_curry, 3)
-print(sum3_curry(1)(2)(3))    # 6
-print(sum3_uncurry(1, 2, 3))  # 6
+sum3_curry = curry(sum3, 3, 1)
+sum3_uncurry = uncurry(sum3_curry, 2)
+print(sum3_curry(2)(3))    # 6
+print(sum3_uncurry(2, 3))  # 6
